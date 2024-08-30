d852e02d229cd4555b08d4fa4be2f9240a99f6fd
extrange <29305375+extrange@users.noreply.github.com>
2024-08-30 13:54:53 +0800

    fix logseq

--- overrides.nix
+++ overrides.nix
@@ -4,12 +4,8 @@
 # https://github.com/extrange/nixos-config/blob/main/useful-commands.md
 { lib, pkgs, ... }:
 {
-  nixpkgs.overlays = [
-    (final: prev: {
-      # https://github.com/NixOS/nixpkgs/issues/321540
-      logseq = prev.logseq.override {
-        electron = final.electron_29;
-      };
-    })
-  ];
+  # Logseq https://github.com/NixOS/nixpkgs/issues/321540
+  # This tell nix that only for logseq version 0.10.9, allow the old version of electron
+  # Once logseq updates this line will no longer apply
+  nixpkgs.config.permittedInsecurePackages = lib.optional (pkgs.logseq.version == "0.10.9") "electron-27.3.11";
 }
