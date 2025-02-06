6b55990f7330b3a2876ddf6fb3a7aece95bf711d
extrange <29305375+extrange@users.noreply.github.com>
2025-02-06 12:26:14 +0800

    fix continue extension

--- home.nix
+++ home.nix
@@ -126,6 +126,24 @@
 
     # Prettier shell prompt
     starship.enable = true;
+
+    vscode = {
+      # See settings here
+      # https://github.com/nix-community/home-manager/blob/master/modules/programs/vscode.nix
+      enable = true;
+
+      # Workaround for continue.dev extension not working
+      # We use vscode.fhs as continue uses libstdc++
+      package = pkgs.vscode.fhsWithPackages (ps: [
+        (ps.openssh.overrideAttrs (prev: {
+          # Fix remote-ssh not working on vscode.fhs
+          # https://github.com/nix-community/home-manager/issues/322
+          patches = (prev.patches or [ ]) ++ [ ./openssh-nocheckcfg.patch ];
+        }))
+      ]);
+      # Note: sudo doesn't work in vscode.fhs
+      # https://discourse.nixos.org/t/sudo-does-not-work-from-within-vscode-fhs/14227/2
+    };
   };
 
   # Gnome settings
